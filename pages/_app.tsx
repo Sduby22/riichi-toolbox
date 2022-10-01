d84fbf467ac0642c5cd34ddba585ee84e84d85f3
sduby <supersduby@gmail.com>
2022-10-01 18:10:55 +0800

    feat: fix appbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import {
-  CssBaseline,
-  Box,
-  Toolbar,
-  IconButton,
-  Typography,
-  Button,
-  AppBar,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { CssBaseline } from "@mui/material";
 import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -30,25 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              News
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Component {...pageProps} />
-      </Box>
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,78 +1,121 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import {
   AppBar,
   BottomNavigation,
   BottomNavigationAction,
   Box,
-  Card,
-  CardContent,
-  Container,
-  Grid,
   Paper,
   Typography,
+  Toolbar,
+  IconButton,
+  Drawer,
 } from "@mui/material";
 
-import CasinoIcon from "@mui/icons-material/Casino";
 import DescriptionIcon from "@mui/icons-material/Description";
 import HomeIcon from "@mui/icons-material/Home";
 import CategoryIcon from "@mui/icons-material/Category";
 import CalculateIcon from "@mui/icons-material/Calculate";
+import MenuIcon from "@mui/icons-material/Menu";
 
 import { useState } from "react";
-import Cheatsheet from "../src/Cheatsheet";
-import Others from "../src/Others";
-import Home from "../src/Home";
+import Cheatsheet from "../src/components/Cheatsheet";
+import Others from "../src/components/Others";
+import Home from "../src/components/Home";
 
 let foo = 1;
 const Page: NextPage = () => {
   const [value, setValue] = useState("Home");
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const drawerWidth = 240;
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
 
   return (
     <>
       <Head>
         <title>Riichi Toolbox</title>
       </Head>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        {/* AppBar */}
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+          </Toolbar>
+        </AppBar>
 
-      {value === "Home" && <Home />}
-      {value === "Cheatsheet" && <Cheatsheet />}
-      {value === "Others" && <Others />}
-
-      {/* bottom naviation bar */}
-      <Paper
-        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
-        elevation={3}
-      >
-        <BottomNavigation
-          value={value}
-          onChange={(_, newValue) => {
-            setValue(newValue);
-            console.log(newValue);
+        <Drawer
+          variant="temporary"
+          open={drawerOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
           }}
-        >
-          <BottomNavigationAction
-            value="Cheatsheet"
-            label="Cheatsheet"
-            icon={<DescriptionIcon />}
-          />
-          <BottomNavigationAction
-            value="Calculator"
-            label="Calculator"
-            icon={<CalculateIcon />}
-          />
-          <BottomNavigationAction
-            label="Home"
-            value="Home"
-            icon={<HomeIcon />}
-          />
-          <BottomNavigationAction
-            value="Others"
-            label="Others"
-            icon={<CategoryIcon />}
-          />
-        </BottomNavigation>
-      </Paper>
+          sx={{
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        ></Drawer>
+
+        <Box sx={{ display: "flex", flexGrow: 1, p: 3 }}>
+          {value === "Home" && <Home />}
+          {value === "Cheatsheet" && <Cheatsheet />}
+          {value === "Others" && <Others />}
+        </Box>
+
+        {/* bottom naviation bar */}
+        <Box sx={{ display: "flex" }}>
+          <Paper
+            sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+            elevation={3}
+          >
+            <BottomNavigation
+              value={value}
+              onChange={(_, newValue) => {
+                setValue(newValue);
+                console.log(newValue);
+              }}
+            >
+              <BottomNavigationAction
+                value="Cheatsheet"
+                label="Cheatsheet"
+                icon={<DescriptionIcon />}
+              />
+              <BottomNavigationAction
+                value="Calculator"
+                label="Calculator"
+                icon={<CalculateIcon />}
+              />
+              <BottomNavigationAction
+                label="Home"
+                value="Home"
+                icon={<HomeIcon />}
+              />
+              <BottomNavigationAction
+                value="Others"
+                label="Others"
+                icon={<CategoryIcon />}
+              />
+            </BottomNavigation>
+          </Paper>
+        </Box>
+      </Box>
     </>
   );
 };
